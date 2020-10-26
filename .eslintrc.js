f2748ffcd55a04607606bc4e93aa92359eed6bd8
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-10-26 13:17:25 +0100

    add eslint-plugin-svelte3

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,14 @@ module.exports = {
     es2021: true,
     node: true
   },
+  plugins: ["svelte3"],
   extends: "eslint:recommended",
+  overrides: [
+    {
+      files: ["**/*.svelte"],
+      processor: "svelte3/svelte3"
+    }
+  ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module"
