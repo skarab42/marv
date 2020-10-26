38be63d284651fe5a0ee93db424c432813b2f884
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-10-26 12:47:31 +0100

    add open menu in systray

--- app/tray.js
+++ app/tray.js
@@ -1,14 +1,20 @@
 const { app, Tray, Menu } = require("electron");
 const config = require("./config");
+const open = require("open");
 
 const { name, version, icon } = config.app;
 const fingerprint = `${name} v${version}`;
+
+const { host, port } = config.server;
+const address = `http://${host}:${port}`;
 
 let tray = null;
 
 function createMenu() {
   return Menu.buildFromTemplate([
     { label: fingerprint, enabled: false },
+    { type: "separator" },
+    { label: "Open", click: () => open(address) },
     { type: "separator" },
     { label: "Quit", click: () => app.quit() }
   ]);
