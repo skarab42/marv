8773a642f57a0b1c4143dfef90c44791f839af24
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-10-26 13:13:06 +0100

    dynamic components

--- client-src/App.svelte
+++ client-src/App.svelte
@@ -1,5 +1,12 @@
 <script>
   import Landing from "./components/Landing.svelte";
+  import Dashboard from "./components/Dashboard.svelte";
+
+  let component = Landing;
+
+  setTimeout(function () {
+    component = Dashboard;
+  }, 1000);
 </script>
 
-<Landing />
+<svelte:component this={component} />
--- client-src/components/Landing.svelte
+++ client-src/components/Landing.svelte
@@ -1,12 +1,21 @@
 <script>
   import MarvLogo from "../images/marv.svg";
+
+  function connect() {
+    console.log("connect...");
+  }
 </script>
 
-<div class="flex h-screen">
-  <div class="m-auto items-center">
-    <MarvLogo width="200" />
-    <div class="p-5 bg-gray-600 rounded shadow">
-      <h3>Marv</h3>
+<div class="flex h-screen overflow-auto">
+  <div class="m-auto">
+    <div class="flex flex-col items-center space-y-5">
+      <MarvLogo width="200" />
+      <div class="font-bold text-2xl">
+        Welcome home!
+      </div>
+      <button class="p-5 bg-purple-700 rounded" on:click={connect}>
+        Enter to the Twitch game
+      </button>
     </div>
   </div>
 </div>
