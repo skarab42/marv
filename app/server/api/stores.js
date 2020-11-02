7c21e5ecc5c3221ff0c63b566f99807eb9ab9df3
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-02 07:38:46 +0100

    Update stores.js

--- app/server/api/stores.js
+++ app/server/api/stores.js
@@ -2,6 +2,9 @@ const stores = require("../../stores");
 
 module.exports = (/* socket */) => {
   return {
+    app(method, ...args) {
+      return stores.app[method](...args);
+    },
     i18next(method, ...args) {
       return stores.i18next[method](...args);
     },
