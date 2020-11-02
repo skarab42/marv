a6a620e7a699c54907c1c3a427abe1dea7b10009
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-02 08:22:10 +0100

    load app store

--- client-src/components/App.svelte
+++ client-src/components/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import { on } from "@/libs/socket.io";
   import i18next from "@/libs/i18next";
+  import appStore from "@/stores/app";
 
   import Connected from "@/components/App/Connected.svelte";
   import Connecting from "@/components/App/Connecting.svelte";
@@ -12,6 +13,7 @@
     component = Connecting;
 
     await i18next();
+    await appStore();
 
     setTimeout(() => {
       if (component !== Disconnected) {
