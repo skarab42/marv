2efc944234511c0adf1fc469a311f9e5cb22da0e
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-03 07:58:42 +0100

    connexion state (color/toggle)

--- client-src/components/OBS/Connect.svelte
+++ client-src/components/OBS/Connect.svelte
@@ -1,13 +1,31 @@
 <script>
   import obs from "@/libs/obs";
-  // import { connected, connecting } from "@/stores/obs";
+  import { state } from "@/stores/obs";
   import Icon from "@/components/UI/Icon.svelte";
   import MdPowerSettingsNew from "svelte-icons/md/MdPowerSettingsNew.svelte";
+
+  function toggle() {
+    if ($state.connected || $state.connecting) {
+      obs.disconnect();
+    } else {
+      obs.connect();
+    }
+  }
+
+  function color({ connected, connecting }) {
+    if (connected) {
+      return "text-green-600 hover:text-red-600";
+    } else if (connecting) {
+      return "text-orange-600 hover:text-red-600";
+    } else {
+      return "hover:text-green-600";
+    }
+  }
 </script>
 
 <div
-  on:click="{obs.connect}"
-  class="flex p-2 space-x-2 hover:text-green-500 cursor-pointer"
+  on:click="{toggle}"
+  class="flex p-2 space-x-2 cursor-pointer {color($state)}"
 >
   <Icon icon="{MdPowerSettingsNew}" />
   <div class="hidden md:inline">OBS</div>
