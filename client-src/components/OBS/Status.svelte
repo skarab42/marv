d551e949f7c17791847d289c28878a110ee8bf9e
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-03 14:44:31 +0100

    show stream/record status

--- client-src/components/OBS/Status.svelte
+++ client-src/components/OBS/Status.svelte
@@ -2,6 +2,20 @@
   import { _ } from "@/libs/i18next";
   import { state } from "@/stores/obs";
 
+  let fps = "0";
+  let cpu = "0";
+  let mem = "0";
+
+  let off = _("words.off");
+
+  let md = "hidden sm:block";
+  let badge = "px-2 bg-primary-dark text-light-darker rounded";
+
+  function timecode(type) {
+    const timecode = $state.status[`${type}-timecode`];
+    return (timecode && timecode.split(".")[0]) || off;
+  }
+
   function label({ connected, connecting }) {
     if (connected) {
       return _("words.connected");
@@ -12,14 +26,38 @@
     }
   }
 
-  $: console.log($state);
+  $: if ($state.streaming) {
+    fps = parseInt($state.status["fps"]);
+    mem = parseInt($state.status["memory-usage"]);
+    cpu = parseFloat($state.status["cpu-usage"]).toFixed(1);
+  }
+
+  $: streamingTimecode = $state.streaming ? timecode("stream") : off;
+  $: recordingTimecode = $state.recording ? timecode("rec") : off;
 </script>
 
-{#if !$state.connected}
-  <div class="truncate">
-    <span>OBS</span>
-    <span class="lowercase">{label($state)}</span>
-  </div>
-{:else}
-  <div class="">status...</div>
-{/if}
+<div class="flex p-2 pl-0 space-x-2">
+  {#if !$state.connected}
+    <div class="truncate">
+      <span>OBS</span>
+      <span class="lowercase">{label($state)}</span>
+    </div>
+  {/if}
+
+  {#if $state.connected}
+    <div class="{badge}">Rec.: {recordingTimecode}</div>
+    <div class="{badge}">Live: {streamingTimecode}</div>
+  {/if}
+
+  {#if $state.connected && $state.streaming}
+    <div class="{badge} {md}">FPS {fps}</div>
+    <div class="{badge} {md}">
+      MEM
+      <span class="inline-block">{mem} MB</span>
+    </div>
+    <div class="{badge} {md}">
+      CPU
+      <span class="inline-block">{cpu} %</span>
+    </div>
+  {/if}
+</div>
