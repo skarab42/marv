b37581469f971ed4631182ccde206869e0a260fe
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-04 11:29:32 +0100

    show/scroll in panels bar

--- client-src/components/Panels/Topbar/Selector.svelte
+++ client-src/components/Panels/Topbar/Selector.svelte
@@ -1,17 +1,60 @@
 <script>
   import api from "@/libs/panels";
-  import { panels } from "@/stores/panels";
+  import { _ } from "@/libs/i18next";
+
   import MdAdd from "svelte-icons/md/MdAdd.svelte";
   import Button from "@/components/UI/Button.svelte";
+  import MdSettings from "svelte-icons/md/MdSettings.svelte";
+  import HorizontalScroller from "@/components/UI/HorizontalScroller.svelte";
+
+  import {
+    panels,
+    currentPanel,
+    setCurrentPanel,
+    editMode,
+    toggleEditMode,
+  } from "@/stores/panels";
+
+  let scoller = null;
 
   $: console.log("$panels:", $panels);
+  $: console.log("$currentPanel", $currentPanel);
+
+  api.on("add", (panel, { owner }) => {
+    owner && scoller.scrollRight();
+  });
+
+  function isActiveClass(p1, p2) {
+    return p1.id === p2.id ? "bg-secondary" : "bg-primary";
+  }
 </script>
 
 <div class="p-2 flex space-x-2 items-center bg-dark text-light">
   <Button
+    padding="p-2"
     icon="{MdAdd}"
-    padding="{null}"
     on:click="{api.add}"
     class="bg-primary"
   />
+  {#if $panels.length}
+    <Button
+      padding="p-2"
+      icon="{MdSettings}"
+      on:click="{toggleEditMode}"
+      class="{$editMode ? 'bg-red-600' : 'bg-primary'}"
+    />
+    <HorizontalScroller bind:this="{scoller}" gap="2" arrowClass="bg-dark">
+      {#each $panels as panel}
+        <Button
+          padding="p-2"
+          on:click="{setCurrentPanel.bind(null, panel)}"
+          class="flex-shrink-0 {isActiveClass($currentPanel, panel)}"
+        >
+          {panel.name}
+        </Button>
+      {/each}
+    </HorizontalScroller>
+  {:else}
+    <div class="p-2">{_('sentences.no-panels-found')}</div>
+  {/if}
 </div>
--- client-src/stores/panels.js
+++ client-src/stores/panels.js
@@ -2,21 +2,33 @@ import { writable } from "svelte/store";
 import api from "@/libs/panels";
 
 export const panels = writable([]);
+export const editMode = writable(false);
+export const currentPanel = writable(null);
 
 let loaded = false;
 
+export function toggleEditMode() {
+  editMode.update((enabled) => !enabled);
+}
+
+export function setCurrentPanel(panel) {
+  currentPanel.set(panel);
+}
+
+function onAdd(panel, { owner }) {
+  panels.update((state) => [...state, panel]);
+  owner && setCurrentPanel(panel);
+}
+
 function loadOnce() {
   if (loaded) return;
-
-  api.on("add", (panel, event) => {
-    console.log("add:", { panel, event });
-  });
-
+  api.on("add", onAdd);
   loaded = true;
 }
 
 export default async function load() {
   const store = await api.getStore();
   panels.set(store.panels);
+  setCurrentPanel(store.panels[0]);
   loadOnce();
 }
