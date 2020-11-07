4bfe87d298e80c568416153f2a09d7485075addf
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-07 09:40:24 +0100

    check file type on upload

--- app/server/libs/files.js
+++ app/server/libs/files.js
@@ -1,8 +1,11 @@
 const path = require("path");
+const mime = require("mime");
 const fs = require("fs-extra");
 const stores = require("../../stores");
 
 const uploadDir = stores.server.get("uploadDir");
+
+const allowedMimeTypes = ["text", "image", "audio", "video"];
 
 fs.ensureDirSync(uploadDir);
 
@@ -10,8 +13,27 @@ function cleanFileName(name) {
   return name.replace(/[^a-z0-9_.]+/gi, "_");
 }
 
-function upload({ name, buffer }) {
+function getFileType(filename) {
+  const ext = path.extname(filename).slice(1);
+  const mimeType = mime.getType(filename);
+
+  if (!ext || !mimeType) {
+    return false;
+  }
+
+  const type = mimeType.split("/")[0];
+
+  return { ext, type, mimeType };
+}
+
+async function upload({ name, buffer }) {
   const filename = cleanFileName(name);
+  const fileType = getFileType(filename);
+
+  if (!allowedMimeTypes.includes(fileType.type)) {
+    return Promise.reject("Disallowed file type");
+  }
+
   const filePath = path.join(uploadDir, filename);
 
   if (fs.existsSync(filePath)) {
