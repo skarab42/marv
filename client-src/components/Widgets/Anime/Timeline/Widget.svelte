86b9f7e0b856d588284190637b029a08e5d3840a
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-15 14:41:45 +0100

    actions feedback

--- client-src/components/Widgets/Anime/Timeline/Widget.svelte
+++ client-src/components/Widgets/Anime/Timeline/Widget.svelte
@@ -1,5 +1,7 @@
 <script>
   import { push, on } from "@/libs/actions";
+  import Icon from "@/components/UI/Icon.svelte";
+  import MdAutorenew from "svelte-icons/md/MdAutorenew.svelte";
   import WidgetWrapper from "@/components/Widgets/WidgetWrapper.svelte";
 
   export let widget;
@@ -42,6 +44,27 @@
   });
 </script>
 
-<WidgetWrapper widget="{widget}" on:click="{triggerAction}">
-  <div class="p-2">running: {clickCount}/{running}</div>
-</WidgetWrapper>
+<WidgetWrapper
+  widget="{widget}"
+  on:click="{triggerAction}"
+  class="cursor-pointer bg-opacity-25 bg-black hover:bg-transparent"
+/>
+
+{#if clickCount}
+  {#if running}
+    <div class="absolute inset-0 pointer-events-none">
+      <div class="h-full animate-spin opacity-50">
+        <MdAutorenew />
+      </div>
+    </div>
+  {/if}
+  <div class="absolute inset-0 pointer-events-none">
+    <div class="flex items-center h-full opacity-50 text-2xl">
+      <div class="flex-auto text-center">
+        <span
+          class="{running ? '' : 'px-3 text-dark bg-gray-500 rounded-full'}"
+        >{clickCount}</span>
+      </div>
+    </div>
+  </div>
+{/if}
