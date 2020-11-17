682a8e674fe5326308fc89b4dd589dca424c628e
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-11-17 15:56:52 +0100

    moved action feedback spinner

--- client-src/components/Widgets/Anime/Timeline/Widget.svelte
+++ client-src/components/Widgets/Anime/Timeline/Widget.svelte
@@ -1,14 +1,8 @@
 <script>
   import { push } from "@/libs/actions";
-  import { store } from "@/stores/actions";
-  import MdAutorenew from "svelte-icons/md/MdAutorenew.svelte";
   import WidgetWrapper from "@/components/Widgets/WidgetWrapper.svelte";
 
   export let widget;
-
-  $: action = $store[widget.id] || {};
-  $: inQueue = action.inQueue || 0;
-  $: running = !!action.running;
 
   function triggerAction() {
     push({ type: "anime", widget }).catch((error) => {
@@ -22,22 +16,3 @@
   on:click="{triggerAction}"
   class="cursor-pointer bg-opacity-25 bg-black hover:bg-transparent"
 />
-
-{#if inQueue}
-  {#if running}
-    <div class="absolute inset-0 pointer-events-none">
-      <div class="h-full animate-spin opacity-50">
-        <MdAutorenew />
-      </div>
-    </div>
-  {/if}
-  <div class="absolute inset-0 pointer-events-none">
-    <div class="flex items-center h-full opacity-50 text-2xl">
-      <div class="flex-auto text-center">
-        <span
-          class="{running ? '' : 'px-3 text-dark bg-gray-500 rounded-full'}"
-        >{inQueue}</span>
-      </div>
-    </div>
-  </div>
-{/if}
--- client-src/components/Widgets/WidgetWrapper.svelte
+++ client-src/components/Widgets/WidgetWrapper.svelte
@@ -1,10 +1,16 @@
 <script>
+  import { store } from "@/stores/actions";
   import Label from "./WidgetLabel.svelte";
+  import MdAutorenew from "svelte-icons/md/MdAutorenew.svelte";
 
   export let widget;
 
   export let cls = "";
   export { cls as class };
+
+  $: action = $store[widget.id] || {};
+  $: inQueue = action.inQueue || 0;
+  $: running = !!action.running;
 </script>
 
 <div
@@ -17,3 +23,22 @@
   </div>
   <slot name="overlay" />
 </div>
+
+{#if inQueue}
+  {#if running}
+    <div class="absolute inset-0 pointer-events-none">
+      <div class="h-full animate-spin opacity-50">
+        <MdAutorenew />
+      </div>
+    </div>
+  {/if}
+  <div class="absolute inset-0 pointer-events-none">
+    <div class="flex items-center h-full opacity-50 text-2xl">
+      <div class="flex-auto text-center">
+        <span
+          class="{running ? '' : 'px-3 text-dark bg-gray-500 rounded-full'}"
+        >{inQueue}</span>
+      </div>
+    </div>
+  </div>
+{/if}
