d695100e2fc516c364ff0d759f3e3e76c59e4e04
skarab42 <skarab@onlfait.ch>
2020-11-22 09:14:04 +0100

    fix: drawer top offset when in electron win

--- client-src/components/Dashboard/Drawer.svelte
+++ client-src/components/Dashboard/Drawer.svelte
@@ -5,13 +5,19 @@
   import OpenOnStartup from "@/components/App/OpenOnStartup.svelte";
   import LanguageSelect from "@/components/App/LanguageSelect.svelte";
   import ConnectAtStartup from "@/components/OBS/ConnectAtStartup.svelte";
+
+  const query = new URLSearchParams(location.search);
+  const electron = query.has("electron");
+
+  let top = electron ? 29 : 0;
 </script>
 
 {#if $drawer.visible}
   <div
     use:clickoutside
     on:clickoutside="{hide}"
-    class="absolute z-50 top-0 right-0 bottom-0 flex flex-col bg-dark shadow"
+    style="top:{top}px"
+    class="absolute z-50 right-0 bottom-0 flex flex-col bg-dark shadow"
   >
     <div class="p-2 bg-secondary">Drawer</div>
     <div class="p-2">{_('words.language')}</div>
