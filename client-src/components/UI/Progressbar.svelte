a55e6db79f42ca6cf922fcf6129d1577edc5c2dc
skarab42 <skarab@onlfait.ch>
2020-11-23 08:17:20 +0100

    Create Progressbar.svelte

---
+++ client-src/components/UI/Progressbar.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let percent;
+  export let height = 4;
+  export let color = "rgba(0,0,0,0.3)";
+  export let bgColor = "transparent";
+</script>
+
+<div style="background-color:{bgColor};">
+  <div
+    style="background-color:{color};width:{percent}%;height:{height}px;"
+  ></div>
+</div>
