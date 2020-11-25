3f4d38127492475fac1a10fe2dd15dcaee25a0a0
skarab42 <skarab@onlfait.ch>
2020-11-25 15:34:27 +0100

    stream status

--- client-src/components/Dashboard/Topbar.svelte
+++ client-src/components/Dashboard/Topbar.svelte
@@ -4,12 +4,14 @@
 
   import OBSConnect from "@/components/OBS/Connect.svelte";
   import OBSStatus from "@/components/OBS/Status.svelte";
+  import StreamStatus from "@/components/Twitch/StreamStatus.svelte";
   import DrawerToggle from "./Drawer/Toggle.svelte";
 </script>
 
 <Flexbar bgColor="bg-primary-darker">
   <OBSConnect />
   <OBSStatus />
+  <StreamStatus />
   <FlexbarSpacer />
   <DrawerToggle />
 </Flexbar>
---
+++ client-src/components/Twitch/StreamStatus.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { state } from "@/stores/twitch";
+  import Icon from "@/components/UI/Icon.svelte";
+  import MdVisibility from "svelte-icons/md/MdVisibility.svelte";
+
+  let badge = "flex space-x-2 px-2 bg-primary-dark text-light-darker rounded";
+</script>
+
+<div class="flex p-2 pl-0 space-x-2">
+  {#if $state.stream}
+    <div class="{badge}">
+      <Icon icon="{MdVisibility}" />
+      <span> {$state.stream.viewer_count}</span>
+    </div>
+  {/if}
+</div>
