25d9505a65ef1923017c8c342d4041e566bbe9d6
skarab42 <skarab@onlfait.ch>
2020-11-26 07:28:43 +0100

    fix: modal top offset when in electron window

--- client-src/components/UI/FullScreenModal.svelte
+++ client-src/components/UI/FullScreenModal.svelte
@@ -1,7 +1,9 @@
 <script>
   import { onMount } from "svelte";
+  import { electron } from "@/stores/app";
 
   let element = null;
+  let top = electron ? 29 : 0;
   let modalWrapper = document.querySelector("#modal");
 
   onMount(() => {
@@ -9,6 +11,10 @@
   });
 </script>
 
-<div bind:this="{element}" class="absolute inset-0 z-50">
+<div
+  bind:this="{element}"
+  class="absolute inset-0 z-50"
+  style="margin-top:{top}px;height: calc(100% - {top}px)"
+>
   <slot />
 </div>
