a9da3aaf05a4d6aeb6fc53a3743b89490a347669
skarab42 <skarab@onlfait.ch>
2020-11-27 20:23:11 +0100

    get follows and get last follows helpers

---
+++ app/server/libs/twitch/api/getLastFollows.js
@@ -0,0 +1,32 @@
+const twitch = require("../index");
+const Viewer = require("../../../db/Models/Viewer");
+
+module.exports = async function getLastFollows() {
+  const user = await twitch.api.helix.users.getMe(true);
+  const followsPaginated = await twitch.api.helix.users.getFollowsPaginated({
+    followedUser: user,
+  });
+
+  const newFollows = [];
+
+  let follows = await followsPaginated.getNext();
+
+  for (let i = 0, l = follows.length; i < l; i++) {
+    const { _data } = follows[i];
+
+    const oldFollow = await Viewer.findByPk(_data.from_id);
+
+    if (oldFollow) break;
+
+    const newFollow = await Viewer.create({
+      id: _data.from_id,
+      name: _data.from_name,
+      followedAt: _data.followed_at,
+      isFollowing: true,
+    });
+
+    newFollows.push(newFollow);
+  }
+
+  return newFollows;
+};
