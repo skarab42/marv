5dd3496f8c3bef0b3edb06c75653bf7c24b34c1f
skarab42 <skarab@onlfait.ch>
2020-11-27 20:52:14 +0100

    add isFirstStart flag

--- app/utils.js
+++ app/utils.js
@@ -1,15 +1,20 @@
 const { name, version } = require("./package");
 const envPaths = require("env-paths");
+const fs = require("fs-extra");
 const path = require("path");
 
-const appPath = __dirname;
 const app = { name, version };
+
+const appPath = __dirname;
 const userPath = envPaths(name).data;
 const clientPath = path.join(appPath, "client");
 const staticPath = path.join(appPath, "static");
 const uploadPath = path.join(userPath, "upload");
 const storesPath = path.join(userPath, "stores");
 const filesPath = path.join(uploadPath, "files");
+const databasePath = path.join(userPath, "database");
+
+const isFirstStart = !fs.existsSync(databasePath);
 const watch = process.argv.includes("--watch") || process.argv.includes("-w");
 
 module.exports = {
@@ -22,4 +27,6 @@ module.exports = {
   storesPath,
   staticPath,
   clientPath,
+  databasePath,
+  isFirstStart,
 };
