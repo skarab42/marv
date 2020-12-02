42bc3bc0ebc46c1a281e3b1082209196a12ad39c
skarab42 <skarab@onlfait.ch>
2020-12-02 16:39:55 +0100

    add Switch component

---
+++ front-src/client/components/UI/Switch.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let enabled = false;
+
+  $: color = enabled ? "bg-green-500" : "bg-red-500";
+  $: position = enabled ? "right-0" : "left-0";
+
+  function onClick() {
+    enabled = !enabled;
+    dispatch("change", enabled);
+  }
+</script>
+
+<div
+  on:click="{onClick}"
+  class="relative h-4 flex items-center rounded cursor-pointer"
+>
+  <div class="w-6 h-2 bg-gray-500 bg-opacity-75 rounded-full"></div>
+  <div
+    class="absolute top-0 {position} w-4 h-4 {color} rounded-full shadow"
+  ></div>
+</div>
