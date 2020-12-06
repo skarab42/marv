3e3f5121978060ed04d546d59963028cd4ede23b
skarab42 <skarab@onlfait.ch>
2020-12-06 10:55:55 +0100

    add refresh button

--- front-src/client/components/Widgets/Twitch/Followers/Widget.svelte
+++ front-src/client/components/Widgets/Twitch/Followers/Widget.svelte
@@ -1,8 +1,10 @@
 <script>
+  import api from "@/api/twitch";
   import { _ } from "@/libs/i18next";
   import Follower from "./Follower.svelte";
   import { followers } from "@/stores/twitch";
   import Button from "@/components/UI/Button.svelte";
+  import MdRefresh from "svelte-icons/md/MdRefresh.svelte";
   import WidgetWrapper from "@/components/Widgets/WidgetWrapper.svelte";
 
   export let widget = null;
@@ -17,11 +19,22 @@
   function select(option) {
     selected = option;
   }
+
+  async function onRefresh() {
+    $followers = await api.getLastFollowers();
+  }
 </script>
 
 <WidgetWrapper widget="{widget}">
   <div class="flex flex-col h-full">
-    <div class="p-2 flex space-x-1">
+    <Button
+      icon="{MdRefresh}"
+      on:click="{onRefresh}"
+      class="w-full p-2 bg-white bg-opacity-25"
+    >
+      <span>{_('words.refresh')}</span>
+    </Button>
+    <div class="p-2 flex space-x-1 items-center">
       {#each options as option}
         <Button
           on:click="{select.bind(null, option)}"
--- front-src/client/components/Widgets/Twitch/Rewards/Widget.svelte
+++ front-src/client/components/Widgets/Twitch/Rewards/Widget.svelte
@@ -1,13 +1,27 @@
 <script>
+  import api from "@/api/twitch";
   import { _ } from "@/libs/i18next";
   import Reward from "./Reward.svelte";
   import { rewards } from "@/stores/twitch";
+  import Button from "@/components/UI/Button.svelte";
+  import MdRefresh from "svelte-icons/md/MdRefresh.svelte";
   import WidgetWrapper from "@/components/Widgets/WidgetWrapper.svelte";
 
   export let widget;
+
+  async function onRefresh() {
+    $rewards = await api.getRewardList();
+  }
 </script>
 
 <WidgetWrapper widget="{widget}">
+  <Button
+    icon="{MdRefresh}"
+    on:click="{onRefresh}"
+    class="w-full p-2 bg-white bg-opacity-25"
+  >
+    <span>{_('words.refresh')}</span>
+  </Button>
   <div
     class="flex flex-col h-full overflow-auto divide-y divide-gray-900 divide-opacity-25"
   >
