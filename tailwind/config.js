ca30e81aaf2ba04647991ff4fb23f92807d70b4b
skarab42 <skarab@onlfait.ch>
2020-12-06 08:07:44 +0100

    custom borders radius

--- tailwind/config.js
+++ tailwind/config.js
@@ -11,6 +11,16 @@ module.exports = {
     extend: {
       colors,
     },
+    borderRadius: {
+      none: "0",
+      sm: "2px",
+      default: "4px",
+      md: "8px",
+      lg: "16px",
+      xl: "42px",
+      half: "50%",
+      full: "100%",
+    },
   },
   variants: {},
   plugins: [],
