f634db54864859bf2a73565c6fddb1ce0c8ee792
skarab42 <skarab@onlfait.ch>
2020-12-07 07:37:13 +0100

    sort commands list by name asc.

--- app/server/libs/twitch/api/getCommandByName.js
+++ app/server/libs/twitch/api/getCommandByName.js
@@ -1,5 +1,5 @@
 const Command = require("../../../db/Models/Command");
 
 module.exports = async function getCommandByName(name) {
-  return await Command.findOne({ where: { name } });
+  return await Command.findOne({ where: { name }, order: [["name", "ASC"]] });
 };
--- app/server/libs/twitch/api/getCommandList.js
+++ app/server/libs/twitch/api/getCommandList.js
@@ -1,5 +1,5 @@
 const Command = require("../../../db/Models/Command");
 
 module.exports = async function getCommandList() {
-  return await Command.findAll();
+  return await Command.findAll({ order: [["name", "ASC"]] });
 };
