6b1ba0aead891fac961b7b8ea7d598f8c969e1e3
skarab42 <skarab@onlfait.ch>
2020-12-10 17:39:02 +0100

    fix: update inverted value

--- front-src/client/components/App/CheckUpdate.svelte
+++ front-src/client/components/App/CheckUpdate.svelte
@@ -6,20 +6,31 @@
   import { version } from "../../../../package";
   import compareVersions from "compare-versions";
   import Modal from "@/components/UI/Modal.svelte";
+  import Button from "@/components/UI/Button.svelte";
   import CheckUpdateCheckbox from "./CheckUpdateCheckbox.svelte";
 
   const repo = "skarab42/marv";
 
+  let downloded = false;
   let newRelease = null;
   let hasNewVersion = false;
 
   async function checkUpdate() {
     newRelease = await getLastRelease(repo);
-    hasNewVersion = !compareVersions.compare(newRelease.version, version, ">");
+    hasNewVersion = compareVersions.compare(newRelease.version, version, ">");
   }
 
   function closeModal() {
     hasNewVersion = false;
+  }
+
+  function download() {
+    downloded = true;
+    window.open(newRelease.url);
+  }
+
+  function closeApp() {
+    api.quit();
   }
 
   if ($store.checkUpdateAtStartup) {
@@ -33,13 +44,26 @@
       <div class="p-5 pr-10 font-bold bg-primary-dark text-light rounded-t">
         {_('sentences.new-version-available')}
       </div>
-      <div class="flex justify-center p-5">
-        <a
-          href="{newRelease.url}"
-          class="p-2 bg-secondary text-light font-bold rounded uppercase"
-        >{_('words.download')}
-          {newRelease.version}</a>
-      </div>
+      {#if downloded}
+        <div
+          class="flex space-x-2 justify-center p-5 bg-orange-500 text-light font-bold"
+        >
+          <span class="uppercase">{_('words.warning')}</span>
+          <span>{_('sentences.close-marv-before-update')}</span>
+        </div>
+        <div class="flex justify-center p-5">
+          <Button class="bg-red-600" on:click="{closeApp}">
+            {_('sentences.close-app')}
+          </Button>
+        </div>
+      {:else}
+        <div class="flex justify-center p-5">
+          <Button class="bg-secondary" on:click="{download}">
+            {_('words.download')}
+            {newRelease.version}
+          </Button>
+        </div>
+      {/if}
       <div class="p-5">
         <CheckUpdateCheckbox />
       </div>
