3a164176d4ef7db9856079a29b44375da301d36b
skarab42 <skarab@onlfait.ch>
2020-12-12 16:16:34 +0100

    add settings seeder

---
+++ app/server/db/seeders/20201212134826-default-settings.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const scope = [
+  "user:read:email",
+  "chat:read",
+  "chat:edit",
+  "channel:read:redemptions",
+  "channel:manage:redemptions",
+];
+
+function setting(key, value, type) {
+  const now = new Date();
+  return { key, value, type, createdAt: now, updatedAt: now };
+}
+
+module.exports = {
+  up: async ({ context }) => {
+    await context.bulkInsert("Settings", [
+      setting("app.language", "en", "string"),
+      setting("app.openOnStartup", "true", "boolean"),
+      setting("app.showFirstStartInfo", "true", "boolean"),
+      setting("app.checkUpdateAtStartup", "true", "boolean"),
+
+      setting("server.host", "localhost", "string"),
+      setting("server.port", "4242", "integer"),
+
+      setting("obs.host", "localhost", "string"),
+      setting("obs.port", "4444", "integer"),
+      setting("obs.password", "", "string"),
+      setting("obs.connectOnStartup", "false", "boolean"),
+
+      setting("twitch.connectOnStartup", false, "boolean"),
+      setting("twitch.currentChannel", "", "string"),
+      setting("twitch.accessToken", "", "string"),
+      setting("twitch.scope", JSON.stringify(scope), "json"),
+    ]);
+  },
+
+  down: async () => {},
+};
--- app/server/db/umzug.js
+++ app/server/db/umzug.js
@@ -1,11 +1,27 @@
 const { Umzug, SequelizeStorage } = require("umzug");
 const { watch } = require("../../utils");
 const sequelize = require("./index");
-const path = require("path");
 
-module.exports = new Umzug({
+const migrations = new Umzug({
   context: sequelize.getQueryInterface(),
-  migrations: { glob: path.join(__dirname, "migrations/*.js") },
-  storage: new SequelizeStorage({ sequelize }),
+  migrations: { glob: ["migrations/*.js", { cwd: __dirname }] },
+  storage: new SequelizeStorage({
+    sequelize,
+    modelName: "SequelizeMigrations",
+  }),
   logger: watch ? console : undefined,
 });
+
+const seeders = new Umzug({
+  context: sequelize.getQueryInterface(),
+  migrations: { glob: ["seeders/*.js", { cwd: __dirname }] },
+  storage: new SequelizeStorage({ sequelize, modelName: "SequelizeSeeders" }),
+  logger: watch ? console : undefined,
+});
+
+module.exports = {
+  async up() {
+    await migrations.up();
+    await seeders.up();
+  },
+};
