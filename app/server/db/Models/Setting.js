6f4748cd2d41d6bebf0a5258ec120181e00108fd
skarab42 <skarab@onlfait.ch>
2020-12-13 08:24:44 +0100

    add setters/getters

--- app/server/db/Models/Setting.js
+++ app/server/db/Models/Setting.js
@@ -1,6 +1,22 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../index");
 
+const getters = {
+  string: (value) => `${value}`,
+  integer: (value) => parseInt(value),
+  float: (value) => parseFloat(value),
+  json: (value) => JSON.parse(value),
+  boolean: (value) => (value === "true" ? true : false),
+};
+
+const setters = {
+  string: (value) => `${value}`,
+  integer: (value) => `${value}`,
+  float: (value) => `${value}`,
+  json: (value) => JSON.stringify(value),
+  boolean: (value) => (value ? "true" : "false"),
+};
+
 const Setting = sequelize.define("Setting", {
   key: {
     type: DataTypes.STRING,
@@ -10,10 +26,12 @@ const Setting = sequelize.define("Setting", {
     type: DataTypes.STRING,
     allowNull: false,
     get() {
-      return this.getDataValue("value");
+      const getter = getters[this.type] || getters.string;
+      return getter(this.getDataValue("value"));
     },
     set(value) {
-      this.setDataValue("value", value);
+      const setter = setters[this.type] || setters.string;
+      this.setDataValue("value", setter(value));
     },
   },
   type: {
