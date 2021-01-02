379c57923addb22e773adf22aedf2b14fa8eec42
skarab42 <skarab@onlfait.ch>
2021-01-02 17:13:35 +0100

    add donate button

--- front-src/client/components/Dashboard/Drawer.svelte
+++ front-src/client/components/Dashboard/Drawer.svelte
@@ -2,8 +2,10 @@
   import { _ } from "@/libs/i18next";
   import { electron } from "@/stores/app";
   import { drawer, hide } from "@/stores/drawer";
+  import GiHearts from "svelte-icons/gi/GiHearts.svelte";
   import clickoutside from "@/libs/svelte/click-outside";
   import HelpLinks from "@/components/App/HelpLinks.svelte";
+  import FaCcPaypal from "svelte-icons/fa/FaCcPaypal.svelte";
   import OpenOnStartup from "@/components/App/OpenOnStartup.svelte";
   import LanguageSelect from "@/components/App/LanguageSelect.svelte";
   import OBSConnectAtStartup from "@/components/OBS/ConnectAtStartup.svelte";
@@ -17,8 +19,8 @@
   <div
     use:clickoutside
     on:clickoutside="{hide}"
-    style="top:{top}px"
-    class="absolute z-50 right-0 bottom-0 flex flex-col bg-dark shadow"
+    style="top:{top}px; max-width:300px;"
+    class="absolute z-50 right-0 bottom-0 flex flex-col bg-dark shadow overflow-y-auto"
   >
     <div class="p-2">{_('words.language')}</div>
     <LanguageSelect />
@@ -27,5 +29,21 @@
     <TwitchConnectAtStartup />
     <CheckUpdateCheckbox />
     <HelpLinks />
+    <dir class="flex-auto"></dir>
+    <div class="p-2">
+      <a
+        class="p-2 flex items-center space-x-5 justify-center bg-pink-600 rounded"
+        href="https://paypal.me/skarab"
+        target="_blank"
+      >
+        <div class="relative truncate px-5 text-4xl">
+          {_('words.donate')}
+          <span class="absolute top-0 right-0 w-6 h-6"><GiHearts /></span>
+        </div>
+        <div class="w-20">
+          <FaCcPaypal />
+        </div>
+      </a>
+    </div>
   </div>
 {/if}
