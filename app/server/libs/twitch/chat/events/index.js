764cb20b87943c2048421a45ac4096a23e9878f1
skarab42 <skarab@onlfait.ch>
2021-01-02 16:11:39 +0100

    extracted events names in own json file

--- app/server/libs/twitch/chat/events/index.js
+++ app/server/libs/twitch/chat/events/index.js
@@ -2,13 +2,8 @@ const twitch = require("../../index");
 const path = require("path");
 const fs = require("fs");
 
-const eventNames = ["onFollow", "onCommand", "onRedemption", "onBits"];
-
 fs.readdirSync(__dirname).forEach((filename) => {
   const name = path.parse(filename).name;
   if (name === "index") return;
-  eventNames.push(name);
   twitch.chat[name](require(`./${name}`).bind(twitch.chat));
 });
-
-module.exports = { eventNames };
