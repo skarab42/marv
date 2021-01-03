e994a93eb7ae53815b7f6acb3cd3cdb87ce74dea
skarab42 <skarab@onlfait.ch>
2021-01-03 10:21:44 +0100

    main menu cosmetics

--- front-src/client/components/App/HelpLinks.svelte
+++ front-src/client/components/App/HelpLinks.svelte
@@ -4,13 +4,19 @@
   import MdHelp from "svelte-icons/md/MdHelp.svelte";
 </script>
 
-<div class="p-2 flex space-x-2 font-bold tracking-wider bg-secondary">
-  <div class="w-6 h-6 ">
+<div class="mt-2 p-2 flex space-x-2 font-bold tracking-wider bg-secondary">
+  <div class="w-6 h-6">
     <MdHelp />
   </div>
-  <div>{_('words.help')}</div>
+  <div class="uppercase">{_('words.help')}</div>
 </div>
 
-{#each links as link}
-  <a class="p-2" href="{url(link.url)}" target="_blank">{_(link.label)}</a>
-{/each}
+<div class="flex flex-col p-1 space-y-1">
+  {#each links as link}
+    <a
+      class="px-2 hover:text-pink-600"
+      href="{url(link.url)}"
+      target="_blank"
+    >{_(link.label)}</a>
+  {/each}
+</div>
