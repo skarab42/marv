e4884580ad7883cbc1edce234ddeb7bede7ee551
skarab42 <skarab@onlfait.ch>
2021-01-06 14:46:09 +0100

    update docs links

--- front-src/client/libs/docs.js
+++ front-src/client/libs/docs.js
@@ -2,13 +2,21 @@ import { store } from "@/stores/app";
 import { get } from "svelte/store";
 
 // TODO get from db settings
-let docsURL = `http://localhost:3000`;
+let docsURL = `https://marv-site.web.app/`;
 
-export const links = [
-  { url: "get-started#obs", label: "docs.get-started.obs" },
-  { url: "get-started#overlay", label: "docs.get-started.overlay" },
-];
+const languages = ["en", "fr"];
+
+const getStartedLinks = {
+  en: `/get-started/`,
+  fr: `/demarrage-rapide/`,
+};
 
 export function url(uri) {
-  return `${docsURL}/${get(store).language}/docs/${uri}`;
+  let lang = get(store).language;
+  lang = languages.includes(lang) ? lang : languages[0];
+  return `${docsURL}/${lang}/docs/${uri}`;
+}
+
+export function getStartURL() {
+  return url(getStartedLinks[get(store).language] || getStartedLinks.en);
 }
