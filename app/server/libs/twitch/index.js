5b3715a2ec462d58d63e27934ac10c74b1d3b303
skarab42 <skarab@onlfait.ch>
2021-01-13 12:53:53 +0100

    feat(twitch-auth): Set redirect URL from database

--- app/server/libs/twitch/index.js
+++ app/server/libs/twitch/index.js
@@ -12,10 +12,17 @@ const api = {
 };
 
 api.init = async function init() {
+  const host = await settings.get("server.host");
+  const port = await settings.get("server.port");
   const scope = await settings.get("twitch.scope", []);
   const accessToken = await settings.get("twitch.accessToken", null);
-
-  api.authProvider = new AuthProvider({ ...config, accessToken, scope });
+  const redirectURI = `http://${host}:${port}/${config.redirectPath}`;
+  api.authProvider = new AuthProvider({
+    ...config,
+    redirectURI,
+    accessToken,
+    scope,
+  });
   api.api = new ApiClient({ authProvider: api.authProvider });
   api.chat = new ChatClient(api.authProvider, {
     requestMembershipEvents: true,
