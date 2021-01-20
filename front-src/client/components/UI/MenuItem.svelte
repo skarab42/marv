ff8a8da8d65115e181fbf9c3817883e9dce1889c
skarab42 <skarab@onlfait.ch>
2021-01-20 17:17:31 +0100

    feat(ui): Add Menu and MenuItem components

---
+++ front-src/client/components/UI/MenuItem.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Icon from "@/components/UI/Icon.svelte";
+
+  export let icon = null;
+</script>
+
+<div
+  on:click
+  class="flex gap-2 p-2 font-medium	hover:bg-pink-700 hover:text-gray-300 capitalize cursor-pointer"
+>
+  {#if icon}
+    <div>
+      <Icon icon="{icon}" />
+    </div>
+  {/if}
+  <div>
+    <slot />
+  </div>
+</div>
