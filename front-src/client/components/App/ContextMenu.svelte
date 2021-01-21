8646a1bdefb0abc30ec8b2717269d204bea1e4c0
skarab42 <skarab@onlfait.ch>
2021-01-21 09:13:28 +0100

    feat(ui): Add ContextMenu component

---
+++ front-src/client/components/App/ContextMenu.svelte
@@ -0,0 +1,16 @@
+<script>
+  import ContextMenu from "@/components/UI/ContextMenu.svelte";
+  import EditModeItem from "./EditModeItem.svelte";
+  import Menu from "@/components/UI/Menu.svelte";
+</script>
+
+<ContextMenu let:opened class="flex flex-col h-full">
+  <slot opened="{opened}" />
+  <div slot="menu">
+    <Menu>
+      <slot name="items" />
+      <hr class="border-b-1 border-gray-800 border-opacity-50" />
+      <EditModeItem />
+    </Menu>
+  </div>
+</ContextMenu>
---
+++ front-src/client/components/App/EditModeItem.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { editMode, toggleEditMode } from "@/stores/panels";
+  import MenuItem from "@/components/UI/MenuItem.svelte";
+  import { _ } from "@/libs/i18next";
+
+  import MdCheck from "svelte-icons/md/MdCheck.svelte";
+  import MdEdit from "svelte-icons/md/MdEdit.svelte";
+
+  $: editModeLabel = $editMode ? _("words.done") : _("sentences.edit-mode");
+  $: editModeIcon = $editMode ? MdCheck : MdEdit;
+</script>
+
+<MenuItem icon="{editModeIcon}" on:click="{toggleEditMode}">
+  {editModeLabel}
+</MenuItem>
