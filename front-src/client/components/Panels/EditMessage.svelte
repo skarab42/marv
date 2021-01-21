548c5407efd031af5062aa9b84be5beda4181ee4
skarab42 <skarab@onlfait.ch>
2021-01-21 15:02:54 +0100

    feat(ui): Add click to open right menu message

---
+++ front-src/client/components/Panels/EditMessage.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { currentPanel } from "@/stores/panels";
+  import { _ } from "@/libs/i18next";
+
+  $: hasWidgets = $currentPanel.widgets.length;
+</script>
+
+{#if !hasWidgets}
+  <div class="absolute inset-0 flex">
+    <div class="p-5 m-auto text-center text-gray-800 text-6xl">
+      <div>{_('sentences.right-click-to-show-menu')}</div>
+    </div>
+  </div>
+{/if}
