e4c2911147da15dfd5adf08ed1b18a66958b62ff
skarab42 <skarab@onlfait.ch>
2021-01-21 11:46:47 +0100

    feat(ui): Add class prop

--- front-src/client/components/UI/MenuItem.svelte
+++ front-src/client/components/UI/MenuItem.svelte
@@ -2,18 +2,22 @@
   import Icon from "@/components/UI/Icon.svelte";
 
   export let icon = null;
+
+  let cls = "";
+
+  export { cls as class };
 </script>
 
 <div
   on:click
-  class="flex gap-2 p-2 font-medium	hover:bg-pink-700 hover:text-gray-300 capitalize cursor-pointer"
+  class="flex gap-2 p-2 font-medium	hover:bg-pink-700 hover:text-gray-300 cursor-pointer"
 >
   {#if icon}
     <div>
       <Icon icon="{icon}" />
     </div>
   {/if}
-  <div>
+  <div class="{cls}">
     <slot />
   </div>
 </div>
