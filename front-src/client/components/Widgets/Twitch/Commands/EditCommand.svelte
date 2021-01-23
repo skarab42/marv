858d8e91c266ea140d7fded2997574b8595eb730
skarab42 <skarab@onlfait.ch>
2021-01-23 17:16:56 +0100

    feat(chat-command): Add EJS support

--- front-src/client/components/Widgets/Twitch/Commands/EditCommand.svelte
+++ front-src/client/components/Widgets/Twitch/Commands/EditCommand.svelte
@@ -3,13 +3,40 @@
   import { _ } from "@/libs/i18next";
   import Modal from "@/components/UI/Modal.svelte";
   import Input from "@/components/UI/Input.svelte";
+  import { debounce } from "throttle-debounce";
+  import CodeMirror from "codemirror";
+  import { onMount } from "svelte";
+
+  import "codemirror/mode/htmlembedded/htmlembedded";
+  import "codemirror/lib/codemirror.css";
+  import "codemirror/theme/material-palenight.css";
 
   export let command;
+
+  let codeArea = null;
 
   function update(key, { target }) {
     command[key] = target.value.trim();
     api.updateCommand(command);
   }
+
+  const updateDebounce = debounce(500, update);
+
+  onMount(() => {
+    const myCodeMirror = CodeMirror.fromTextArea(codeArea, {
+      mode: "application/x-ejs",
+      lineNumbers: true,
+      theme: "material-palenight",
+      tabSize: 2,
+    });
+
+    myCodeMirror.on("changes", () => {
+      const value = myCodeMirror.getValue();
+      updateDebounce("message", { target: { value } });
+    });
+  });
+
+  $: message = command.message || "";
 </script>
 
 <Modal closable="{true}" on:close>
@@ -38,14 +65,24 @@
         value="{command.cooldown}"
         on:change="{update.bind(null, 'cooldown')}"
       />
-      <div class="p-2 text-gray-700 uppercase text-xl font-bold">
-        {_('words.message')}
+      <div
+        class="p-2 flex items-center gap-2 text-gray-700 uppercase text-xl font-bold"
+      >
+        <div>{_('words.message')}</div>
+        <div class="text-xs">
+          (
+          <a class="text-pink-600" href="https://ejs.co/#docs" target="_blank">
+            {_('sentences.supports-ejs-syntax')}
+          </a>
+          )
+        </div>
       </div>
       <textarea
         class="p-2 text-dark"
+        bind:this="{codeArea}"
         on:change="{update.bind(null, 'message')}"
         style="min-width:50vw; min-height:250px"
-      >{command.message || ''}</textarea>
+      >{message}</textarea>
     </div>
   </div>
 </Modal>
