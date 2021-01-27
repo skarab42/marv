b328661602a6eb0196cd4d868105c3466ac33408
skarab42 <skarab@onlfait.ch>
2021-01-27 15:12:26 +0100

    feat: Add twitch pubsub auto reconnection

--- app/server/libs/twitch/pubsub/index.js
+++ app/server/libs/twitch/pubsub/index.js
@@ -2,29 +2,43 @@ const { BasicPubSubClient, PubSubClient } = require("twitch-pubsub-client");
 const onRedemption = require("./onRedemption");
 const onBits = require("./onBits");
 const twitch = require("../index");
+const state = require("../state");
 
-const reconnectDelay = 5;
+let connectId = null;
+const reconnectDelay = 5; // seconds
 
 const rootClient = new BasicPubSubClient();
 const pubSubClient = new PubSubClient(rootClient);
 
-function reconnect() {
-  console.log(`Reconnecting...`);
-  pubSubClient._rootClient.reconnect();
+function delayConnect() {
+  connectId && clearTimeout(connectId);
+  connectId = setTimeout(reconnect, reconnectDelay * 1000);
 }
 
+function reconnect() {
+  state.set("pubsub.connected", false);
+  rootClient
+    .reconnect()
+    .then(() => {
+      state.set("pubsub.connected", true);
+    })
+    .catch(() => {
+      delayConnect();
+    });
+}
+
+rootClient.onDisconnect((manually) => {
+  state.set("pubsub.connected", false);
+  if (!manually) delayConnect();
+});
+
 module.exports = async function init() {
+  state.set("pubsub.connected", false);
+
   const userId = await pubSubClient.registerUserListener(twitch.api);
 
   await pubSubClient.onRedemption(userId, onRedemption);
   await pubSubClient.onBits(userId, onBits);
 
-  pubSubClient._rootClient.onDisconnect((manually, reason) => {
-    // if (manually) return;
-    console.log("PUBSUB ERROR >>>", reason);
-    console.log(`Reconnecting in ${reconnectDelay} sec.`);
-    setTimeout(reconnect, reconnectDelay * 1000);
-  });
-
-  setTimeout(() => pubSubClient._rootClient.disconnect(), 5000);
+  state.set("pubsub.connected", true);
 };
