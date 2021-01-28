abdc54585dde56f1def3589d74f68afd7b7901ff
Philippe Gibert <philippe.gibert@gmail.com>
2021-01-28 22:27:17 +0100

    Fix quit behaviour on MacOs

--- app/main/window/hideOnClose.js
+++ app/main/window/hideOnClose.js
@@ -1,10 +1,25 @@
 const { app } = require("electron");
+const quit = require("../quit");
+
+
 
 module.exports = function hideOnClose(win) {
+  let willQuitApp = false;
+
   win.on("close", (event) => {
+
+    // Quit on Cmd + Q on Mac
+    if (willQuitApp && process.platform === 'darwin') {
+      quit();
+    }
+
     if (!app.__isQuitting) {
       event.preventDefault();
       win.hide();
     }
   });
+
+  // Detect If the user decide to quit app
+  app.on('before-quit', () => willQuitApp = true);
+
 };
