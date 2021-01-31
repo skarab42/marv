cb9974a555f000dff876913248aaa2dff6b99d3c
skarab42 <skarab@onlfait.ch>
2021-01-31 14:09:35 +0100

    feat(ui): Notify edit mode is active in topbar

--- front-src/client/components/Dashboard/Drawer/Toggle.svelte
+++ front-src/client/components/Dashboard/Drawer/Toggle.svelte
@@ -2,11 +2,13 @@
   import { toggle } from "@/stores/drawer";
   import MdMenu from "svelte-icons/md/MdMenu.svelte";
   import Icon from "@/components/UI/Icon.svelte";
+
+  export let bgColor = "bg-primary";
 </script>
 
 <div
   on:click|stopPropagation="{toggle}"
-  class="p-2 bg-primary hover:bg-secondary cursor-pointer"
+  class="p-2 {bgColor} hover:bg-secondary cursor-pointer"
 >
   <Icon icon="{MdMenu}" />
 </div>
--- front-src/client/components/Dashboard/Topbar.svelte
+++ front-src/client/components/Dashboard/Topbar.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { _ } from "@/libs/i18next";
+  import { editMode } from "@/stores/panels";
   import DrawerToggle from "./Drawer/Toggle.svelte";
   import Flexbar from "@/components/UI/Flexbar.svelte";
   import OBSStatus from "@/components/OBS/Status.svelte";
@@ -6,13 +8,22 @@
   import TwitchLogin from "@/components/Twitch/Login.svelte";
   import FlexbarSpacer from "@/components/UI/Flexbar/Spacer.svelte";
   import StreamStatus from "@/components/Twitch/StreamStatus.svelte";
+
+  $: bgColor = $editMode ? "bg-pink-700" : "bg-primary-darker";
+  $: toggleBgColor = $editMode ? "bg-pink-700" : "bg-primary";
 </script>
 
-<Flexbar bgColor="bg-primary-darker">
-  <OBSConnect />
-  <OBSStatus />
-  <StreamStatus />
-  <FlexbarSpacer />
+<Flexbar bgColor="{bgColor}">
+  {#if $editMode}
+    <div class="p-2 flex-auto font-bold text-center uppercase">
+      {_('sentences.edit-mode')}
+    </div>
+  {:else}
+    <OBSConnect />
+    <OBSStatus />
+    <StreamStatus />
+    <FlexbarSpacer />
+  {/if}
   <TwitchLogin />
-  <DrawerToggle />
+  <DrawerToggle bgColor="{toggleBgColor}" />
 </Flexbar>
