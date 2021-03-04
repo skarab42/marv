4b0006928eb33c3f40fb8dfb3ee0e6a01a697b04
skarab42 <skarab@bluewin.ch>
2021-03-04 17:41:27 +0100

    feat: Add quit button in main menu (#210)

---
+++ front-src/client/components/Dashboard/QuitButton.svelte
@@ -0,0 +1,24 @@
+<script>
+  import app from "@/api/app";
+  import { _ } from "@/libs/i18next";
+  import Button from "@/components/UI/Button.svelte";
+  import ConfirmModal from "@/components/UI/ConfirmModal.svelte";
+
+  let confirmModal = false;
+
+  function quit() {
+    confirmModal = true;
+  }
+
+  function onConfirm({ detail }) {
+    detail && app.quit();
+    confirmModal = false;
+  }
+</script>
+
+<Button class="bg-red-600" on:click="{quit}">{_('words.quit')}</Button>
+<ConfirmModal
+  opened="{confirmModal}"
+  on:confirm="{onConfirm}"
+  question="{_('sentences.confirm-quit')}"
+/>
