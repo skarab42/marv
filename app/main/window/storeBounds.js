9687a0af1e5dc9731be236b5dc999450ceae9224
Alvin Pergens <arubinu@free.fr>
2021-03-30 17:02:28 +0200

    feat: Store fullscreen state (#240)

--- app/main/window/storeBounds.js
+++ app/main/window/storeBounds.js
@@ -12,8 +12,18 @@ module.exports = function storeBounds({ win, name, delay = 500 } = {}) {
 
   let timeout = null;
 
-  const loadBounds = () => win.setBounds(store.get("bounds"));
-  const updateBounds = () => store.set("bounds", win.getBounds());
+  const loadBounds = () => {
+    win.setBounds(store.get("bounds"));
+    if (store.get("fullscreen")) win.maximize();
+  };
+
+  const updateBounds = () => {
+    const maximized = win.isMaximized();
+
+    store.set("fullscreen", maximized);
+    if ( !maximized )
+      store.set("bounds", win.getBounds());
+  };
 
   const updateBoundsDelay = () => {
     timeout && clearTimeout(timeout);
