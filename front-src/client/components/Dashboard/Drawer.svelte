357984f5a8787e36d925ebb3393bb3f467d952cf
skarab42 <skarab@bluewin.ch>
2021-03-30 15:35:17 +0200

    feat(OBS): Edit port and password (#238)

--- front-src/client/components/Dashboard/Drawer.svelte
+++ front-src/client/components/Dashboard/Drawer.svelte
@@ -5,6 +5,7 @@
   import { drawer, hide } from "@/stores/drawer";
   import clickoutside from "@/libs/svelte/click-outside";
   import HelpLinks from "@/components/App/HelpLinks.svelte";
+  import OBSSettings from "@/components/OBS/SettingsModal.svelte";
   import OpenOnStartup from "@/components/App/OpenOnStartup.svelte";
   import LanguageSelect from "@/components/App/LanguageSelect.svelte";
   import OBSConnectAtStartup from "@/components/OBS/ConnectAtStartup.svelte";
@@ -21,14 +22,15 @@
     style="top:{top}px; max-width:300px;"
     class="absolute z-50 right-0 bottom-0 flex flex-col bg-dark shadow overflow-y-auto"
   >
+    <QuitButton />
     <LanguageSelect />
+    <OBSSettings />
     <div class="flex flex-col divide-y divide-gray-800 divide-opacity-50">
       <OpenOnStartup />
       <OBSConnectAtStartup />
       <TwitchConnectAtStartup />
       <CheckUpdateCheckbox />
     </div>
-    <QuitButton />
     <HelpLinks />
     <div class="flex-auto"></div>
     <Donate />
