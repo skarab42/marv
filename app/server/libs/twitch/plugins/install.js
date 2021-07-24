17df34e9b74c97829ce0e03d00305b742ff8f854
skarab42 <skarab@bluewin.ch>
2021-07-24 14:36:24 +0200

    fix: fetch follows

--- app/server/libs/twitch/plugins/install.js
+++ app/server/libs/twitch/plugins/install.js
@@ -1,8 +1,6 @@
 const updateFollowsPlugin = require("./updateFollowsPlugin");
 const streamStatePlugin = require("./streamState");
 const followsPlugin = require("./followsPlugin");
-const appApi = require("../../../api/app");
-const { _ } = require("../../i18next");
 
 let installLock = false;
 
@@ -11,12 +9,7 @@ module.exports = async function install() {
 
   installLock = true;
 
-  try {
-    await updateFollowsPlugin();
-  } catch (error) {
-    appApi.stateNotify("error", _("errors.unable_to_fetch_follows"));
-  }
-
-  streamStatePlugin();
-  followsPlugin();
+  await updateFollowsPlugin();
+  await streamStatePlugin();
+  await followsPlugin();
 };
